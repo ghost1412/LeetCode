f123e28a12bc6d4abbca40616bcfc424f3fceb74
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2021-01-26 19:07:59 +0000

    [LeetCode Sync] Runtime - 20 ms (32.67%), Memory - 7.7 MB (100.00%)

---
+++ my-folder/0237-delete-node-in-a-linked-list/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node;
+        while(temp&&temp->next){
+            temp->val = temp->next->val;
+            if(temp->next->next == NULL)
+                temp->next = NULL;
+            cout<<"a";
+            temp = temp->next;
+
+        }
+        temp = NULL;
+    }
+};
