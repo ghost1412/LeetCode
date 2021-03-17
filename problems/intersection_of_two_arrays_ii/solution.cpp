255f5033ca6c7eea366a88a7075899d50aa81d0c
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2021-03-17 16:57:27 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (cpp)

---
+++ problems/intersection_of_two_arrays_ii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> mp1, mp2;
+        for(auto i : nums1)
+            mp1[i]++;
+        for(auto j: nums2)
+            mp2[j]++;
+        vector<int> ans;
+        for(auto i : mp1){
+            if(mp2.find(i.first) != mp2.end()){
+                int temp = min(i.second, mp2[i.first]);
+                    while(temp--){
+                        ans.push_back(i.first);
+                    }
+            }
+        }
+        return ans;
+    }
+};
