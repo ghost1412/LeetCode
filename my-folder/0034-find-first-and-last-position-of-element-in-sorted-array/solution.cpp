7ae3cb68e6a2bcab0fbb37b70f74068f7f71ecd3
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2021-05-09 12:10:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.7 MB (100.00%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size()==0) return {-1, -1};
-        if(nums.size()==1&&nums[0]!=target) return {-1, -1};
-        int s = 0, e = nums.size()-1;
-        while(s<e){
-            int m = (s+e)/2;
-            if(s+1==e&&nums[s]!=target&&nums[e]!=target){
-                s=-1;
-                e=-1;
+        int n = nums.size();
+        if(n == 0)
+            return vector<int>{-1, -1};
+        if(n == 1)
+            return (nums[0] == target)? vector<int>{0, 0}: vector<int>{-1, -1};
+        int s = 1, e = n - 1, mid = s + (e - s)/2, first = 0;
+        while(s <= e){
+            if(nums[mid-1] < target && nums[mid] == target){
+                first = mid;
                 break;
-            }             
-            if(nums[s]==target && nums[e]==target)
-                break;
-            if(nums[m]==target && nums[m+1]==target && nums[m-1]==target){
-                if(nums[s]!=target) s++;
-                if(nums[e]!=target) e--;
-            }else if(nums[m]==target && nums[m+1]!=target)
-                e = m;
-            else if(nums[m]==target && nums[m-1]!=target)
-                s = m;
-            else if(nums[m]>target) e=m;
-            else if(nums[m]<target && m!=s) s=m;
-            else if(nums[m]<target && m==s) s=m+1;
+            }
+            else if(nums[mid] < target)
+                s = mid + 1;
+            else 
+                e = mid - 1;
+            mid = s + (e - s)/2;
         }
-        return {s, e};
+        if(first == 0 && target != nums[0]) return vector<int>{-1, -1};
+        s = first;
+        e = n - 2;
+        mid = s + (e - s)/2;
+        int last = n - 1;
+        while(s <= e){
+            if(nums[mid] == target && nums[mid+1] > target){
+                last = mid;
+                break;
+            }
+            else if(nums[mid] > target){
+                e = mid - 1;
+            }
+            else
+                s = mid + 1;
+            mid = s + (e - s)/2;
+        }
+        return vector<int>{first, last};
     }
 };
