2854da29f500ec1122bfb1fbc00ec133df94784f
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2021-06-21 18:24:59 +0000

    Sync LeetCode submission - Distribute Candies (cpp)

--- problems/distribute_candies/solution.cpp
+++ problems/distribute_candies/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        int n = set<int>(candyType.begin(), candyType.end()).size();
+        int n = unordered_set<int> (candyType.begin(), candyType.end()).size();
         return n > candyType.size()/2? candyType.size()/2 : n;
     }
 };
