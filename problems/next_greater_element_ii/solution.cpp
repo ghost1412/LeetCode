9b33a1a84cda6cc721d629bd5a3b52d4379a2077
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2022-11-27 17:51:26 +0000

    Sync LeetCode submission - Next Greater Element II (cpp)

---
+++ problems/next_greater_element_ii/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> s;
+        vector<int> ans(nums.size(), -1);
+        int n = nums.size();
+        for (int i=0; i < 2*n - 1; i++) {
+            while (!s.empty() && nums[s.top()] < nums[i%n]) {
+                ans[s.top()] = nums[i%n];
+                s.pop();
+            }
+            s.push(i%n);
+        }
+        return ans;
+    }
+};
+
