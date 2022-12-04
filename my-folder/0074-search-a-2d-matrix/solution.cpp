4caf780c300e34a2c362fbaba1f03e7f71a600e0
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2022-12-04 16:30:25 +0000

    [LeetCode Sync] Runtime - 9 ms (0.27%), Memory - 9.7 MB (100.00%)

--- my-folder/0074-search-a-2d-matrix/solution.cpp
+++ my-folder/0074-search-a-2d-matrix/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    bool binarySearch(vector<int> arr, int l, int r, int x)
-    {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-            if (arr[mid] == x){
-                cout<<mid;
-                return 1;
-            }
-            if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-
-            return binarySearch(arr, mid + 1, r, x);
+    
+    bool binarySearch(vector<int> m, int target, int l, int r) {
+        if (l <= r) {
+            int mid = l + (r - l)/2;
+            if (m[mid] == target) return true;
+            else if (m[mid] > target) return binarySearch(m, target, l, mid-1);
+            else return binarySearch(m, target, mid+1, r);
         }
-        return 0;
+        return false;
     }
+    
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0; i<matrix.size(); i++){
-            if(target == matrix[i][matrix[i].size()-1])
+        for (int i=0; i < matrix.size(); i++) {
+            if (target == matrix[i][matrix[i].size() - 1]) {
                 return true;
-            else
-                if(target <  matrix[i][matrix[i].size()-1])
-                    return binarySearch(matrix[i], 0, matrix[i].size()-1, target);
+            } else {
+                if (target < matrix[i][matrix[i].size() - 1]) {
+                    return binarySearch(matrix[i], target, 0, matrix[i].size() - 1);
+                }
+            }
         }
         return false;
     }
