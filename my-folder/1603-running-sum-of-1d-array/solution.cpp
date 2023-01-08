8f473ee8ebdc801d43cfbe084a1d76bc7f585ecb
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2023-01-08 16:13:56 +0000

    [LeetCode Sync] Runtime - 4 ms (1.56%), Memory - 8.6 MB (100.00%)

--- my-folder/1603-running-sum-of-1d-array/solution.cpp
+++ my-folder/1603-running-sum-of-1d-array/solution.cpp
@@ -2,10 +2,13 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int> ans;
-        int sum = 0;
-        for(int i=0; i<nums.size(); i++){
-            sum += nums[i];
-            ans.push_back(sum);
+        for (int i=0; i<nums.size(); i++) {
+            if (ans.size() == 0) {
+                ans.push_back(nums[i]);
+                continue;
+            }
+
+            ans.push_back(nums[i] + ans[ans.size() - 1]);
         }
         return ans;
     }
