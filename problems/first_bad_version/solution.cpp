b9c39afd8b00293cf0b5e4172cd1c105cb47c62d
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2023-01-08 15:22:01 +0000

    Sync LeetCode submission - First Bad Version (cpp)

--- problems/first_bad_version/solution.cpp
+++ problems/first_bad_version/solution.cpp
@@ -1,21 +1,25 @@
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
+
+
 class Solution {
 public:
-    
-    int binarySearch(int l, int r) {
-        if (l > r) return -1;
-        int mid = l + (r - l)/2;
-        if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
-        else if (isBadVersion(mid)) {
-            return binarySearch(l, mid - 1);
+
+    int isBadVersionBinarySearch(int l, int r) {
+        int mid = l + (r-l) /2 ;
+        bool isBadCurrent = isBadVersion(mid);
+        bool isBadPrev = isBadVersion(mid-1);
+        if (isBadCurrent && !isBadPrev) return mid;
+        if (isBadCurrent && isBadPrev) {
+            return isBadVersionBinarySearch(l, mid - 1);
         } else {
-            return binarySearch(mid + 1, r);
+            return isBadVersionBinarySearch(mid + 1, r);
         }
+
     }
-    
+
     int firstBadVersion(int n) {
-        return binarySearch(0, n);
+        return isBadVersionBinarySearch(0, n);
     }
 };
