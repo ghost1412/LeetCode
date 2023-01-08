c95510551a96f243ec7d0e70a4a1992841223bf6
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2023-01-08 15:31:50 +0000

    [LeetCode Sync] Runtime - 6 ms (0.10%), Memory - 9.5 MB (100.00%)

--- my-folder/0035-search-insert-position/solution.cpp
+++ my-folder/0035-search-insert-position/solution.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    
-    int binarySearch(vector<int>& nums, int target, int l, int r) {
+
+    int binarySearchInsert(vector<int>& nums, int target, int l, int r) {
         if (l > r) {
             return l;
         }
         int mid = l + (r - l)/2;
         if (nums[mid] == target) return mid;
         else if(nums[mid] > target) {
-            return binarySearch(nums, target, l, mid-1);
+            return binarySearchInsert(nums, target, l, mid-1);
         } else {
-            return binarySearch(nums, target, mid+1, r);
+            return binarySearchInsert(nums, target, mid+1, r);
         }
     }
-    
+
     int searchInsert(vector<int>& nums, int target) {
-       return  binarySearch(nums, target, 0, nums.size() - 1);
+        return binarySearchInsert(nums, target, 0, nums.size() - 1);
     }
 };
