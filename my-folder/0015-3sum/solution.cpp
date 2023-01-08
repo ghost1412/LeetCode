ec695bb7f34bb507eb5aab9bb88f3670c36f38a3
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2023-01-08 15:52:42 +0000

    [LeetCode Sync] Runtime - 144 ms (15.47%), Memory - 21 MB (100.00%)

--- my-folder/0015-3sum/solution.cpp
+++ my-folder/0015-3sum/solution.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
         if(nums.size()<3){
             return {};
         }
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
-        for(int k=0; k<nums.size()-2; k++){
-            if(k!=0 && nums[k]==nums[k-1]) continue;
-            int i=k+1;
-            int j=nums.size()-1;
-            while(i<j){
-                if(nums[i]+nums[j]+nums[k]==0){
-                    vector<int> r;
-                    r.push_back(nums[k]);
-                    r.push_back(nums[i]);
-                    r.push_back(nums[j]);
-                    ans.push_back(r);
-                    int left = nums[i];
-                    int right = nums[j];
-                    while(i<j && left==nums[i])
-                        i++;
-                    while(i<j && right==nums[j])
-                        j--;
+        for (int i=0; i<nums.size(); i++) {
+            if (i!=0 && nums[i] == nums[i-1]) continue;
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while (j < k) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    vector<int> trip = {nums[i], nums[j], nums[k]};
+                    ans.push_back(trip);
+                    int left = nums[j];
+                    int right = nums[k];
+                    while (j < k && nums[j] == left) {
+                        j++;
+                    }
+                    while (j < k && nums[k] == right) {
+                        k--;
+                    }
+                } else if (nums[i] + nums[j] + nums[k] > 0){
+                    k--;
+                } else {
+                    j++;
                 }
-                else if(nums[i]+nums[j]+nums[k]>0){
-                       j--;
-                }
-                else
-                       i++;
             }
         }
         return ans;
