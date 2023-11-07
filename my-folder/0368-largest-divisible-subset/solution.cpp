0735a2f395d9cad5bd889d35dfcd4c3721a124e5
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2023-11-07 18:41:23 +0000

    [LeetCode Sync] Runtime - 35 ms (9.15%), Memory - 9.2 MB (100.00%)

--- my-folder/0368-largest-divisible-subset/solution.cpp
+++ my-folder/0368-largest-divisible-subset/solution.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         int n = nums.size();
         vector<int> dp(n, 1);
-        sort(nums.begin(), nums.end());
-        for(int i=1; i<nums.size(); i++){
-            for(int j=0; j<i; j++){
-                if(nums[i]%nums[j]==0){
+
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<i; j++) {
+                if (nums[i] % nums[j] == 0) {
                     dp[i] = max(dp[i], dp[j]+1);
                 }
             }
         }
+
         int max_i = 0;
-        for(int i=1; i<n; i++){
-            if(dp[i] > dp[max_i])
+        int count = 0;
+
+        for (int i=1; i<n; i++) {
+            if (dp[i] > dp[max_i]) {
                 max_i = i;
-        }
-        int temp = nums[max_i];
-        int count = dp[max_i];
-        vector<int> ans;
-        for(int i=max_i; i>=0; i--){
-            if(temp%nums[i] == 0 && dp[i] == count){
-                ans.push_back(nums[i]);
-                count--;
-                temp = nums[i];
             }
         }
-        return ans;
+
+        count = dp[max_i];
+        vector<int> res;
+        int maxModEle = nums[max_i];
+        for (int i=max_i; i>=0; i--) {
+            if (maxModEle % nums[i] == 0 && dp[i] == count) {
+                count--;
+                res.push_back(nums[i]);
+                maxModEle = nums[i];
+            }
+        }
+
+        return res;
     }
 };
