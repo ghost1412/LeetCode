64ff37db797cf00baf4aa9a771a943d9b5ebf717
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2023-11-10 09:09:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.4 MB (99.96%)

--- my-folder/0100-same-tree/solution.cpp
+++ my-folder/0100-same-tree/solution.cpp
@@ -11,42 +11,12 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {        
-        stack<TreeNode*> a, b;
-        if(p)
-            a.push(p);
-        if(q)
-            b.push(q);
-        while(!a.empty()&&!b.empty()){
-            if(a.top()->val != b.top()->val)
-                return 0;
-            TreeNode* top = a.top();
-            a.pop();
-            TreeNode* top1 = b.top();
-            b.pop();
-            if(top->left && top1->left || (top->left == NULL && top1->left == NULL)){
-                if(top->left != NULL){
-                    if(top->left->val == top1->left->val){
-                        a.push(top->left);
-                        b.push(top1->left);           
-                    }else
-                        return 0;
-                }
-            }else
-                return 0;
-            if(top->right && top1->right|| (top->right == NULL && top1->right == NULL)){
-                if(top->right != NULL){
-                    if(top->right->val == top1->right->val){
-                        a.push(top->right);
-                        b.push(top1->right);           
-                    }else
-                        return 0;
-                }
-            }else
-                return 0;        
-        }
-        if(a.size()!=b.size())
-            return 0;
-        return 1;
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+
+        return (p->val == q->val) 
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
     }
 };
