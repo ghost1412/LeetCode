9694d9c81c9b27d3dedfe8afe2b6461d3d7d1e0a
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2024-04-23 16:08:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

--- my-folder/0290-word-pattern/solution.cpp
+++ my-folder/0290-word-pattern/solution.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-       vector<string>st(26," ");
-        int j=0;
-        string temp="";
-        vector<string>v;
-        for(int i=0;i<s.length();i++){
-            if(s[i]==' '){
-                v.push_back(temp);
-                temp="";
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        stringstream ss(s);
+        string word;
+        int index = 0;
+
+        while (ss >> word) {
+            if (index >= pattern.size()) {
+                return false; // More words than characters in pattern
             }
-            else{
-                temp+=s[i];
-            }
-        }
-        v.push_back(temp);
-        temp="";
-        if(pattern.length()!=v.size()){
-            return false;
-        }
-        
-        vector<string>visited(26," ");
-        set<string>see;
-        for(int i=0;i<pattern.length();i++){
-            if(st[pattern[i]-'a']!=" "){
-                if(st[pattern[i]-'a']!=v[i]){
-                    return false;
+
+            char c = pattern[index];
+            if (charToWord.find(c) == charToWord.end() && wordToChar.find(word) == wordToChar.end()) {
+                charToWord[c] = word;
+                wordToChar[word] = c;
+            } else {
+                if (charToWord[c] != word || wordToChar[word] != c) {
+                    return false; // Conflict in mapping
                 }
-                
             }
-            else{
-                if(see.find(v[i])!=see.end()){
-                    return false;
-                }
-                st[pattern[i]-'a']=v[i];
-                see.insert(v[i]);
-            }
+
+            index++;
         }
-       return true; 
-        
+
+        return index == pattern.size(); // Check if all characters in pattern were used
     }
 };
+
