ec4f316943f883a91377e2c2f22688441243ce55
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-01-15 15:05:06 +0000

    [LeetCode Sync] Runtime - 11 ms (1.19%), Memory - 12.3 MB (38.79%)

--- my-folder/0088-merge-sorted-array/solution.cpp
+++ my-folder/0088-merge-sorted-array/solution.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int loc = m; 
-        for(int i=m; i<nums1.size(); i++) nums1[i] = INT_MIN;
-        if(loc) loc--;
-        int i = nums1.size()-1, j = nums2.size()-1;
-        while(loc>=0 && j>=0 && i>=0){             
-            if(nums1[loc] > nums2[j]){
-                if(i==loc && j==0){
-                    nums1[i] = nums2[j];
-                    return;
+        int till = (m > n) ? m : n;
+        cout<<m;
+        vector<int> mergedArry;
+        int j=0, k=0, i=0;
+        while(j<m || k<n) {               
+            if (m > j && n > k) {
+                if (nums1[j] < nums2[k]) {
+                    mergedArry.push_back(nums1[j++]);
+                } else {
+                    mergedArry.push_back(nums2[k++]);
                 }
-               // cout<<nums1[loc]<<i;
-                nums1[i--] = nums1[loc];
-                nums1[loc] = INT_MIN;
-                if(loc!=0) loc--;
-            }else{           
-                nums1[i--] = nums2[j--];
+            } else if (m > j) {
+                mergedArry.push_back(nums1[j++]);
+            } else if (n > k) {
+                mergedArry.push_back(nums2[k++]);
             }
+            i++;
+            cout<<j<<k<<" ";
         }
+        nums1 = mergedArry;
     }
 };
