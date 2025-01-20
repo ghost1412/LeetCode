cc9a757f1ddefac40033c4069ae985a04a0425da
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-01-20 16:32:07 +0000

    [LeetCode Sync] Runtime - 3 ms (89.85%), Memory - 23.9 MB (34.12%)

--- my-folder/0056-merge-intervals/solution.cpp
+++ my-folder/0056-merge-intervals/solution.cpp
@@ -1,30 +1,16 @@
-bool operator<(vector<int> a, vector<int> b){
-    return a[0] < b[0];
-}
-
 class Solution {
 public:
-    
-    vector<vector<int>> merge(vector<vector<int>>& a) {
-        vector<vector<int>> ans;
-        sort(a.begin(), a.end());
-        ans.push_back(a[0]);
-        int pos = 0;
-        for(int i=1; i<a.size(); i++){
-            if(ans[pos][1] >= a[i][0]){
-                vector<int> n;
-                n.push_back(ans[pos][0]);
-                if(ans[pos][1] > a[i][1])
-                    n.push_back(ans[pos][1]);
-                else
-                    n.push_back(a[i][1]);
-                ans.pop_back();
-                ans.push_back(n);
-            }else{
-                ans.push_back(a[i]);
-                pos++;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> mergedIntervals;
+        sort(intervals.begin(), intervals.end());
+        mergedIntervals.push_back(intervals[0]);
+        for (int i=1; i<intervals.size(); i++) {
+            if (mergedIntervals.back()[1] < intervals[i][0]) {
+                mergedIntervals.push_back(intervals[i]);
+            } else if (intervals[i][1] > mergedIntervals.back()[1]) {
+                mergedIntervals.back()[1] =  intervals[i][1];
             }
         }
-        return ans;
+        return mergedIntervals;
     }
 };
