feb3a4b08351879d6b5d92144b0d8eaec31054bc
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-01-28 17:39:41 +0000

    [LeetCode Sync] Runtime - 2 ms (70.79%), Memory - 12 MB (40.48%)

---
+++ my-folder/0071-simplify-path/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isDir(string path) {
+        return (path != "/" && path != ".." && path != ".");
+
+    }
+
+    string simplifyPath(string path) {
+        stack<string> s;
+        stringstream ss(path);
+        string token;
+        while (getline(ss, token, '/')) {
+            if (token == "." || token == "" || token == "/") {
+                continue;
+            } else if (token == "..") {
+                if (!s.empty()) 
+                    s.pop();
+            } else {
+                s.push(token);
+            }
+        }
+
+        string result = "";
+        while (!s.empty()) {
+            result = "/" + s.top() + result;
+            s.pop();
+        }
+
+        return result.empty() ? "/" : result;
+    }
+};
