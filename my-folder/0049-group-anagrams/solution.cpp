6b275181c302f5e707fc4fab8eda9856b03602c8
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-01-28 18:00:44 +0000

    [LeetCode Sync] Runtime - 17 ms (54.50%), Memory - 27.6 MB (23.79%)

--- my-folder/0049-group-anagrams/solution.cpp
+++ my-folder/0049-group-anagrams/solution.cpp
@@ -1,17 +1,29 @@
+struct ArrayHash {
+    size_t operator()(const array<int, 26>& arr) const {
+        size_t hashValue = 0;
+        for (int num : arr) {
+            hashValue ^= hash<int>{}(num) + 0x9e3779b9 + (hashValue << 6) + (hashValue >> 2);
+        }
+        return hashValue;
+    }
+};
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        if(strs.size() == 0){
-            return {{}};
+        unordered_map<array<int, 26>, vector<string>, ArrayHash> m;
+
+        for (int i=0; i<strs.size(); i++) {
+            array<int, 26> alpha = {};
+            for (int j=0; j<strs[i].size(); j++) {
+                alpha[strs[i][j]-'a']++;
+            }
+
+            m[alpha].push_back(strs[i]);
         }
-        unordered_map<string, vector<string>> mp;
-        for(auto s: strs){
-            string temp(s);
-            sort(temp.begin(), temp.end());
-            mp[temp].push_back(s);
-        }
+
         vector<vector<string>> res;
-        for(auto s: mp){
+        for(auto s: m){ 
             res.push_back(s.second);
         }
         return res;
