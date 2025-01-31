0fea50151752f9c43347e206bc55142f27c0e6f9
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-01-31 17:24:23 +0000

    [LeetCode Sync] Runtime - 3 ms (49.13%), Memory - 9.9 MB (25.61%)

--- my-folder/0242-valid-anagram/solution.cpp
+++ my-folder/0242-valid-anagram/solution.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return (s==t);
+        vector<int> sFreq(26,0), tFreq(26, 0);
+
+        for (int i=0; i<s.size(); i++) {
+            sFreq[s[i]-'a']++;
+        }
+
+        for (int i=0; i<t.size(); i++) {
+            tFreq[t[i]-'a']++;
+        }
+
+        for (int i=0; i<26; i++) {
+            if (sFreq[i] != tFreq[i]) return false;
+        }
+        return true;
     }
 };
