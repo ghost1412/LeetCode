7d96be5f05be67430f2e9310894343e9a79857fa
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-01-31 16:56:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (65.77%)

--- my-folder/0392-is-subsequence/solution.cpp
+++ my-folder/0392-is-subsequence/solution.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
+
     bool isSubsequence(string s, string t) {
-        int i = 0, j = 0;
-        while(i!=t.length()&&j!=s.length()){
-            //cout<<s[i]<<j<<endl;
-            if(s[j]!=t[i])
-                i++;
-            else{
+        int i=0, j=0, n=t.size(), m=s.size();
+
+        while (j<n && i<m) {
+            if (s[i] != t[j]) {
                 j++;
+            } else {
                 i++;
+                j++;
             }
         }
-        if(j==s.length())
-            return true;
-        return false;
+        return (i==m);
     }
 };
