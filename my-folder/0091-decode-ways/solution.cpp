004aa7034d29803e76cb65dab1972901190cf906
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-02-11 19:38:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (21.61%)

--- my-folder/0091-decode-ways/solution.cpp
+++ my-folder/0091-decode-ways/solution.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int  n = s.size() + 1;
-        vector<int> dp(n, 0);
-
+        vector<int> dp(s.size()+1, 0);
         dp[0] = 1;
         dp[1] = (s[0] == '0') ? 0 : 1;
+        for (int i=2; i<s.size()+1; i++) {
+            int doub = stoi(s.substr(i-2, 2));
+            int sing = stoi(s.substr(i-1, 1));
 
-        for (int i=2; i<n; i++) {
-            int singleSelection = stoi(s.substr(i-1, 1));
-            int doubleSelection = stoi(s.substr(i-2, 2));
+            if (doub >= 10 && doub <= 26) {
+                dp[i] = dp[i-2];
+            }
 
-            if (singleSelection >= 1 && singleSelection <= 9) {
+            if (sing >= 1 && sing <= 9) {
                 dp[i] += dp[i-1];
             }
-
-            if (doubleSelection >= 10 && doubleSelection <= 26) {
-                dp[i] += dp[i-2];
-            }
         }
-        return dp[n-1];
+        return dp[s.size()];
     }
 };
