cc29d0266d7e64831379f575536aa7a83e751810
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-02-26 15:02:55 +0000

    [LeetCode Sync] Runtime - 21 ms (6.95%), Memory - 22.4 MB (5.33%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -9,16 +9,38 @@
  */
 class Solution {
 public:
+
+    unordered_map<TreeNode*, TreeNode*> parent;
+    unordered_map<TreeNode*, int> depth;
+
+    void preprocess(TreeNode* root, TreeNode* par, int d) {
+        if (!root) return;
+        parent[root] = par;
+        depth[root] = d;
+        preprocess(root->left, root, d+1);
+        preprocess(root->right, root, d+1);
+    }
+
+    TreeNode* findLCA(TreeNode* p, TreeNode* q) {
+        while (p!=q) {
+            if (depth[p] > depth[q]) p = parent[p];
+            else q = parent[q];
+        }
+        return p;
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) return NULL;
+        preprocess(root, NULL, 0);
+        return findLCA(p, q);
+        // if (root == NULL) return NULL;
 
-        if (root->val == p->val || root->val == q->val) return root;
+        // if (root->val == p->val || root->val == q->val) return root;
 
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        // TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        // TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
-        if (left && right) return root;
+        // if (left && right) return root;
 
-        return (left) ? left : right;
+        // return (left) ? left : right;
     }
 };
