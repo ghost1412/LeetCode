c24de3fb5bb2506ea1648ddb19f268447f019bff
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-03-03 16:44:38 +0000

    [LeetCode Sync] Runtime - 15 ms (29.33%), Memory - 17.6 MB (20.88%)

--- my-folder/0139-word-break/solution.cpp
+++ my-folder/0139-word-break/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-       unordered_map<string, bool> mp;
-       for (int i=0; i<wordDict.size(); i++){
-          mp[wordDict[i]] = true;
-       }
+        unordered_map<string, bool> mp;
+        for (auto word: wordDict) {
+            mp[word] = true;
+        }
+
         vector<bool> dp(s.size()+1, false);
         dp[0] = true;
         string subStr = "";
-       for (int i=1; i<s.size()+1; i++) {
-            for (int j=0; j<=i; j++) {
+        for (int i=1; i<s.size()+1; i++) {
+            for(int j=0; j<=i; j++) {
                 subStr = s.substr(j, i-j);
                 if (dp[j] && mp.find(subStr) != mp.end()) {
                     dp[i] = true;
                 }
             }
-       }
-       return dp[s.size()];
+        }
+        return dp[s.size()];
     }
 };
