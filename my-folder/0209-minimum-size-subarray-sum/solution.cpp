0d86ce635db6bda4bf8298408ac9cbb84197fc1a
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-04-13 20:37:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 32.1 MB (71.35%)

--- my-folder/0209-minimum-size-subarray-sum/solution.cpp
+++ my-folder/0209-minimum-size-subarray-sum/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    int minSubArrayLen(int s, vector<int>& nums) {
-        int i=0, j=0, sum=0, ans= INT_MAX;
-        int left=0;
-        for(i=0; i<nums.size(); i++){
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int sum=0;
+        int left = 0;
+        int minLen = INT_MAX;
+        for (int i=0; i<nums.size(); i++) {
             sum += nums[i];
-            while(sum>=s){
-                ans = min(ans, i+1-left);
-                sum-=nums[left++];
+            while (sum >= target) {
+                minLen = min(minLen, i-left+1);
+                sum -= nums[left++];
             }
         }
-        return (ans!=INT_MAX) ? ans:0;
+        return (minLen != INT_MAX) ? minLen:0;
     }
 };
