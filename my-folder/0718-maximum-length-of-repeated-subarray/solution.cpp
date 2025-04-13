10c4c96da6b8af708bdcb2c2c779e3f3e91ef41b
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-04-13 20:48:32 +0000

    [LeetCode Sync] Runtime - 127 ms (19.33%), Memory - 89.5 MB (9.52%)

--- my-folder/0718-maximum-length-of-repeated-subarray/solution.cpp
+++ my-folder/0718-maximum-length-of-repeated-subarray/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int findLength(vector<int>& A, vector<int>& B) {
-        int n = A.size(), m = B.size();
-        int dp[n+1][m+1];
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        vector<vector<int>> dp(nums1.size()+1, vector<int>(nums2.size()+1));
         int ans = INT_MIN;
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=m; j++){
-                if(i==0 || j==0) dp[i][j]=0;
-                else if(B[j-1] == A[i-1])
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                else{
+        for (int i=0; i<=nums1.size(); i++) {
+            for (int j=0; j<=nums2.size(); j++) {
+                if (i==0 || j==0) dp[i][j] = 0;
+                else if (nums1[i-1] == nums2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                } else {
                     dp[i][j] = 0;
                 }
                 ans = max(ans, dp[i][j]);
@@ -18,3 +17,12 @@ public:
         return ans;
     }
 };
+
+//   3 2 1 4 7
+//  1 0 0 1 1 1
+//  2 0 1 1 1 1
+//  3 1 1 1 1 1
+//  2 0 2
+//  1
+
+
