fc51c857f2760c75db4ae51c52ecded907544cad
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-04-19 20:43:12 +0000

    [LeetCode Sync] Runtime - 856 ms (16.33%), Memory - 11.7 MB (34.36%)

--- my-folder/0079-word-search/solution.cpp
+++ my-folder/0079-word-search/solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
+    
         for (int i=0; i<board.size(); i++) {
-            for (int j=0; j<board[i].size(); j++) {
+            for (int j=0; j<board[0].size(); j++) {
                 if (board[i][j] == word[0]) {
-                    if (search(board, word, 0, i, j))
-                        return true;
+                    if (search(board, word, 0, i, j)) return true;
                 }
             }
         }
@@ -13,22 +13,15 @@ public:
     }
 
     bool search(vector<vector<char>>& board, string word, int index, int i, int j) {
-        int wordLen = word.size();
-        int n = board.size();
-        int m = board[0].size();
-
-        if (wordLen == index) return true;
-
-        if (i < 0 || j < 0 || i >= n || j >= m || word[index] != board[i][j]) return false;
-
+        if (index == word.size()) return true;
+        if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || word[index] !=  board[i][j]) return false;
+        index++;
         char temp = board[i][j];
         board[i][j] = '*';
-        bool found = search(board, word, index+1, i+1, j) ||
-            search(board, word, index+1, i, j+1) ||
-            search(board, word, index+1, i-1, j) ||
-            search(board, word, index+1, i, j-1);
+        bool found = search(board, word, index, i+1, j) || search(board, word, index, i, j+1)
+        || search(board, word, index, i-1, j) || search(board, word, index, i, j-1);
         board[i][j] = temp;
         return found;
-    }
 
+    }
 };
