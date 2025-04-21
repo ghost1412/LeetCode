d984725d65bdce1c679987d75467e69f3e4bd108
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-04-21 17:31:14 +0000

    [LeetCode Sync] Runtime - 4 ms (34.96%), Memory - 12.2 MB (45.56%)

--- my-folder/0133-clone-graph/solution.cpp
+++ my-folder/0133-clone-graph/solution.cpp
@@ -20,36 +20,18 @@ public:
 */
 
 class Solution {
-
-private:
-unordered_map<int, Node*> nodeRef;
-
 public:
-
-    vector<Node*> copyNeighbors(vector<Node*>& neighbors) {
-        vector<Node*> neighborsCopy;
-
-        for (int i=0; i < neighbors.size(); i++) {
-            Node* copiedNeighbor = copyNodes(neighbors[i]);
-            neighborsCopy.push_back(copiedNeighbor);
-        }
-        return neighborsCopy;
-    }
-
-    Node* copyNodes(Node* node) {
-        if (node == NULL) return nullptr;
-        int val = node->val;
-        if (this->nodeRef.find(val) != this->nodeRef.end()) {
-            return nodeRef[val];
-        }
-        Node* newNode = new Node(val, {});
-        this->nodeRef[val] = newNode;
-
-        newNode->neighbors = copyNeighbors(node->neighbors);
-        return newNode;
-    }
-
+    unordered_map<int, Node*> nodeRef;
     Node* cloneGraph(Node* node) {
-        return copyNodes(node);
+        if (node == NULL) return NULL;
+        if (nodeRef.find(node->val) != nodeRef.end()) return nodeRef[node->val];
+        Node* copy = new Node(node->val);
+        nodeRef[node->val] = copy;
+        for(int i=0; i<node->neighbors.size(); i++) {
+
+            Node* copy2 = cloneGraph(node->neighbors[i]);
+            copy->neighbors.push_back(copy2);
+        }
+        return copy;
     }
 };
