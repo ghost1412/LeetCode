7bfb39f3c4eefc70cb8af312db15243c2209a3b3
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-04-23 20:29:27 +0000

    [LeetCode Sync] Runtime - 91 ms (35.08%), Memory - 48 MB (26.91%)

--- my-folder/0744-network-delay-time/solution.cpp
+++ my-folder/0744-network-delay-time/solution.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<array<int, 2>> graph[n+1];
-        for(vector<int> it: times){
+        vector<vector<pair<int, int>>> graph(n+1);
+
+        for (vector<int> it: times) {
             graph[it[0]].push_back({it[1], it[2]});
         }
-        vector<int> distance(n+1, INT_MAX);
-        distance[k] = 0;
-        priority_queue<array<int, 2>, vector<array<int, 2>>, greater<array<int, 2>>> pq;
+
+        vector<int> distances(n+1, INT_MAX);
+        distances[k]=0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         pq.push({0, k});
-        while(!pq.empty()){
-            int curr = pq.top()[1];
+
+        while(!pq.empty()) {
+            int curr = pq.top().second;
             pq.pop();
-            for(array<int, 2> it: graph[curr]){
-                int next = it[0];
-                int time = it[1];
-                if(distance[next] > distance[curr] + time){
-                    distance[next] = distance[curr] + time;
-                    pq.push({distance[next], next});
+            cout<<curr;
+            for (pair<int,int> it: graph[curr]) {
+                int next = it.first;
+                int time = it.second;
+                if (distances[next] > distances[curr]+time) {
+                    distances[next] = distances[curr]+time;
+                    pq.push({distances[next], next});
                 }
             }
         }
+                
         int ans = 0;
-        for(int i = 1; i<distance.size(); i++) {
-            ans = max(ans, distance[i]);
+        for(int i = 1; i<distances.size(); i++) {
+            ans = max(ans, distances[i]);
         }
         return ans == INT_MAX ? -1 : ans;
     }
