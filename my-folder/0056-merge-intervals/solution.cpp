e37cb617d117804890114c48e5bc6306332875ba
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-05-18 19:42:05 +0000

    [LeetCode Sync] Runtime - 5 ms (54.56%), Memory - 23.8 MB (59.48%)

--- my-folder/0056-merge-intervals/solution.cpp
+++ my-folder/0056-merge-intervals/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>> mergedIntervals;
-        sort(intervals.begin(), intervals.end());
-        mergedIntervals.push_back(intervals[0]);
-        for (int i=1; i<intervals.size(); i++) {
-            if (mergedIntervals.back()[1] < intervals[i][0]) {
-                mergedIntervals.push_back(intervals[i]);
-            } else
-            if (mergedIntervals.back()[1] < intervals[i][1]) {
-                mergedIntervals.back()[1] = intervals[i][1];
-            }  
-        }
-        return mergedIntervals;
+       vector<vector<int>> merged;
+       sort(intervals.begin(), intervals.end());
+
+       merged.push_back(intervals[0]);
+
+       for (int i=1; i<intervals.size(); i++) {
+            if (merged.back()[1] >= intervals[i][0]) {
+                merged.back()[1] = max(intervals[i][1], merged.back()[1]);
+            } else {
+                merged.push_back(intervals[i]);
+            }
+       }
+       return merged;
     }
 };
