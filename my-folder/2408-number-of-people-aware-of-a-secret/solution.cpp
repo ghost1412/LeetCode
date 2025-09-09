dc9df4b4c37f653ee04e03916d572bd9b7dc1bb3
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-09-09 01:03:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.3 MB (71.08%)

---
+++ my-folder/2408-number-of-people-aware-of-a-secret/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long long> dp(n + 1, 0);
+        dp[1] = 1;
+        long share=0, MOD=(long)1e9+7;
+        
+        for (int t=2; t<=n; t++) {
+            if (t-delay > 0) {
+                share = (share+dp[t-delay]+MOD)%MOD;
+            } 
+
+            if (t-forget > 0) {
+                share = (share-dp[t-forget]+MOD)%MOD;
+            }
+            dp[t] = share;
+        }
+
+        long know = 0;
+
+        for(int i=n-forget+1; i<=n; i++) 
+            know = (know+dp[i]) %MOD;
+        return (int)know;
+    }
+};
