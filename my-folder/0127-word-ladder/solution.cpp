796b56a4da02180b187c6413287eeffa52b528ee
Ashutosh Jatav <31374078+ghost1412@users.noreply.github.com>
2025-12-22 05:42:32 +0000

    [LeetCode Sync] Runtime - 1143 ms (7.72%), Memory - 20.9 MB (73.17%)

--- my-folder/0127-word-ladder/solution.cpp
+++ my-folder/0127-word-ladder/solution.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
 
-    bool isOneDiffAway(string a, string b) {
+    bool hasOneDiff(string a, string b) {
         int diff = 0;
         for (int i=0; i<a.size(); i++) {
-            if (a[i]!=b[i]) diff++;
-            if (diff>1) return false;
+            if(a[i] != b[i]) diff++;
         }
-        return diff == 1;
+
+        return (diff==1);
     }
 
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-       // unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        queue<pair<string, int>>  q;
-        unordered_set<string> wordSet(wordList.begin(), wordList.end());
-        if (wordSet.find(endWord) == wordSet.end()) {
-            return 0;
-        }
+        sort(wordList.begin(), wordList.end());
+        unordered_set<string> s(wordList.begin(), wordList.end());
+
+        queue<pair<string, int>> q;
+
         q.push({beginWord, 1});
 
         while (!q.empty()) {
-            auto [currWord, level] = q.front();
+            auto [top, level] = q.front();
             q.pop();
-            for (auto it=wordSet.begin(); it!=wordSet.end(); ) {
+
+            for (auto it=s.begin(); it!=s.end();) {
                 string nextWord = *it;
-                if (isOneDiffAway(currWord, nextWord)) {
+                if (hasOneDiff(top, nextWord)) {
                     if (nextWord == endWord) return level+1;
                     q.push({nextWord, level+1});
-                    it = wordSet.erase(it);
+                    it = s.erase(it);
                 } else {
                     it++;
                 }
